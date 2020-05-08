86ca14db23036ab40cb4af4d4519aaff36da6822
Miguel <miguelef91@gmail.com>
2020-05-08 23:29:26 +0200

    Adds CORS support

--- menu/settings.py
+++ menu/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'adminlte3',
     'adminlte3_theme',
+    'corsheaders',
     'dishes.apps.DishesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -44,6 +45,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+CORS_ORIGIN_ALLOW_ALL = True
