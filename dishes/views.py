d1fbb8a477b4b9b59ebca6c1a60352b8a8375e59
Miguel <miguelef91@gmail.com>
2020-05-08 14:58:20 +0200

    Removes external API creation for dishes, drinks, ingredients and desserts

--- dishes/views.py
+++ dishes/views.py
@@ -5,21 +5,21 @@ from dishes.models import Dish, Ingredient, Drink, Desert
 from dishes.serializers import DishSerializer, IngredientSerializer, DrinkSerializer, DesertSerializer
 
 
-class DishViewSet(GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin):
+class DishViewSet(GenericViewSet, mixins.RetrieveModelMixin, mixins.ListModelMixin):
     serializer_class = DishSerializer
     queryset = Dish.objects.all()
 
 
-class IngredientViewSet(GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin):
+class IngredientViewSet(GenericViewSet, mixins.RetrieveModelMixin, mixins.ListModelMixin):
     serializer_class = IngredientSerializer
     queryset = Ingredient.objects.all()
 
 
-class DrinkViewSet(GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin):
+class DrinkViewSet(GenericViewSet, mixins.RetrieveModelMixin, mixins.ListModelMixin):
     serializer_class = DrinkSerializer
     queryset = Drink.objects.all()
 
 
-class DesertViewSet(GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin):
+class DesertViewSet(GenericViewSet, mixins.RetrieveModelMixin, mixins.ListModelMixin):
     serializer_class = DesertSerializer
     queryset = Desert.objects.all()
