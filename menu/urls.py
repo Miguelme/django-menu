913226aa1ef04a4f490e43a9ff761308c7affe84
Miguel <miguelef91@gmail.com>
2020-05-08 22:14:16 +0200

    Ignores static content and adds logout handling

--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 from rest_framework import routers
 
 from dishes.views import DishViewSet, DrinkViewSet, IngredientViewSet, DesertViewSet
@@ -14,5 +15,6 @@ router.register(r'desert', DesertViewSet, basename='desert')
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls),
+    url(r'^logout/', RedirectView.as_view(url='/admin/logout')),
+    path('admin/', admin.site.urls)
 ]
