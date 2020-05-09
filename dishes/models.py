6c593c18a01b453ac9dc887e3f504b2b541f1f8a
Miguel <miguelef91@gmail.com>
2020-05-09 14:24:27 +0200

    Changes imageUrl as required to non-required

--- dishes/models.py
+++ dishes/models.py
@@ -12,7 +12,7 @@ class Ingredient(models.Model):
 	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=128)
 	category = models.CharField(max_length=64)
-	imageUrl = models.URLField()
+	imageUrl = models.URLField(blank=True)
 
 	def __str__(self):
 		return self.name
@@ -24,7 +24,7 @@ class PriceableModel(models.Model):
 	description = models.CharField(max_length=512)
 	category = models.CharField(max_length=64)
 	price = models.DecimalField(max_digits=16, decimal_places=2)
-	imageUrl = models.URLField()
+	imageUrl = models.URLField(blank=True)
 	notes = models.CharField(max_length=512, blank=True)
 	isAvailable = models.BooleanField(default=True, verbose_name="Is Available?")
 	ingredients = models.ManyToManyField(Ingredient, blank=True, related_name="%(class)singredients_ingredient")
