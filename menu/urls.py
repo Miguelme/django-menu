62362fb3dee843b95b085c8eb4371f3097b1ecf6
Miguel <miguel.fagundez@glovoapp.com>
2021-07-10 14:02:20 +0200

    Upgrades version and adds JWT Support

--- menu/urls.py
+++ menu/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import RedirectView
 from rest_framework import routers
-
+from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 from dishes.views import DishViewSet, DrinkViewSet, IngredientViewSet, DessertViewSet
 
 router = routers.DefaultRouter()
@@ -16,5 +17,7 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^logout/', RedirectView.as_view(url='/admin/logout')),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls)
 ]
